b71f450c03b7212856de93a077be4d96c211ad7e
MichaelToast <michaelp86788@gmail.com>
2024-04-21 15:54:59 -0500

    New Genre Function completed

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ def StandardFavGenresInfo():
              "Drama":0, "Family":0, "Fantasy":0, "History":0, "Horror":0, "Music":0, "Mystery":0, "Romance":0,
                "Science Fiction":0, "Thriller":0, "TV Movie":0, "War":0, "Western":0}
 
-    reviewPageUrl = "https://letterboxd.com/" + str(getAccountName()) + "/films/reviews/"
+    reviewPageUrl = "https://letterboxd.com/" + str(getAccountName()) + "/films/reviews/for/2024/"
     resultReviews = requests.get(reviewPageUrl)
     reviewPage = BeautifulSoup(resultReviews.text, "html.parser")
 
@@ -130,36 +130,70 @@ def StandardFavGenresInfo():
 
 #trying another idea for getting the genres - using the diary instead of the reviews
 
+def newGenrefunc():
+    genreDict = {"Action": 0, "Adventure":0, "Animation":0, "Comedy":0, "Crime":0, "Documentary":0,
+             "Drama":0, "Family":0, "Fantasy":0, "History":0, "Horror":0, "Music":0, "Mystery":0, "Romance":0,
+               "Science Fiction":0, "Thriller":0, "TV Movie":0, "War":0, "Western":0}
 
-diaryURL = "https://letterboxd.com/" + getUserName() + "/films/diary/for/2018" #2024 so its equivalent to PRO page
-print(diaryURL)
-#2018 there is only one page, 2024 there are mutliple
-resultDiary = requests.get(diaryURL)
-diaryPage = BeautifulSoup(resultDiary.text, "html.parser")
+    diaryURL = "https://letterboxd.com/" + getUserName() + "/films/diary/for/2024" #2024 so its equivalent to PRO page
+    #2018 there is only one page, 2024 there are mutliple
+    resultDiary = requests.get(diaryURL)
+    diaryPage = BeautifulSoup(resultDiary.text, "html.parser")
 
-lastPage = diaryPage.findAll("li", attrs={"class":"paginate-page"})
+    lastPage = diaryPage.findAll("li", attrs={"class":"paginate-page"})
+    errorMessage = (diaryPage.find("p", attrs={"class": "ui-block-heading"}).text.strip()).find("logged any")
 
-errorMessage = (diaryPage.find("p", attrs={"class": "ui-block-heading"}).text.strip()).find("logged any")
+    if (errorMessage != -1):
+        print("Users has not reviewed any movies")
+        return
 
-if (errorMessage != -1):
-    print("Users has not reviewed any movies")
-    #return
+    maxPage = 1
+    #getting the maximum pageCount
+    if (len(lastPage) != 0):
+        maxPage = int(lastPage[len(lastPage) - 1].text.strip())
+    else:
+        print("only one page to read")
+    
+    for i in range(1, maxPage + 1):
+        subDiaryUrl = diaryURL + "/page/" + str(i) + "/"
+        resultSubPage = requests.get(subDiaryUrl)
+        subDiary = BeautifulSoup(resultSubPage.text, "html.parser")
+        movieList = subDiary.findAll("div", attrs={"data-film-slug": True})
+        for film in movieList:        
+            moviepageURL = "https://letterboxd.com/film/" + str(film["data-film-slug"]) + "/genres/"
+            resultmoviePage = requests.get(moviepageURL)
+            moviePage = BeautifulSoup(resultmoviePage.text, "html.parser")
 
-maxPage = 1
-#getting the maximum pageCount
-if (len(lastPage) != 0):
-    maxPage = int(lastPage[len(lastPage) - 1].text.strip())
-else:
-    print("only one page to read")
+            #now reading the genres
+            genreBlock = moviePage.find("div", attrs={"class":"text-sluglist capitalize"})
+            if (genreBlock == None):
+                # Film does not have any genres or themes to pull from
+                continue
+            genreNames = (genreBlock.findAll("a", href=True))
+            # Updating the dictionary, if there are no genres/themes, nothing will be added
+            for genre in genreNames:
+                name = str(genre.text.strip())
+                if (genreDict.get(str(name)) != None):
+                    genreDict.update({str(name) : (genreDict.get(str(name))) + 1})
+
+    sortedGenres = sorted(genreDict.items(), key=lambda kv: kv[1], reverse=True)
+    for genre in sortedGenres[:10]:
+        if (genre[1] != 0):
+            print(f"{genre[0]} - {genre[1]}", end = "\0")
+            if (genre[1] == 1):
+                print(" film")
+            else:
+                print(" films")
+        else:
+            print("No Films to read from")
+            break
+
+
+newGenrefunc()
 
 
 
 
-# trying to get the movie name
-movieName = diaryPage.findAll("div", attrs={"data-film-slug": True})
-for film in movieName:        
-    moviePageUrl = "https://letterboxd.com/film/" + str(film["data-film-slug"]) + "/genres/"
-    #print(moviePageUrl)
 
 
 
