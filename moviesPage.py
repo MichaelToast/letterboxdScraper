e2f2876ae5c3adea100e52e0e1c662fe6fadccec
MichaelToast <michaelp86788@gmail.com>
2024-12-17 12:55:23 -0600

    Form Basics completed

--- moviesPage.py
+++ moviesPage.py
@@ -1,13 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+userData = {
+    'pageLink': '',
+    'username': 'Michael',
+    'accountType': 'Standard',
+    'ratings': 12
+}
+
+@app.route('/', methods=["GET", "POST"])
+@app.route('/home', methods=["GET", "POST"])
 def home():
-    return "<h1>Hello There!!!!!!<h1>"
+    if (request.method == "POST"):
+        pageLink = request.form.get("flink") 
+        userData["pageLink"] = pageLink
+        # Now, I have to pass the link through the data collection functions
+        return render_template('data.html', posts=userData)
+    return render_template('home.html')
+
+@app.route('/dataPage')
+def dataPage():
+    return render_template('data.html', posts=userData)
 
 if __name__ == '__main__':
     app.run(debug=True)
 
 
 # to run: flask run
+# to run in debug: python moviesPage.py
